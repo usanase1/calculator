96bab11695c8d09536c5747928896424f12d3051
usanase1 <137633145+usanase1@users.noreply.github.com>
2025-03-24 11:49:05 +0200

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,8 @@
+let num1 = 7;
+let num2 = 6;
+let sum = num1 + num2;
+let product = num1 * num2;
+let diff = num2 - num1;
+let quotient = num1/num2;
+console.log("Sum of  two numbers is :" +sum + "the product is :" +product )
+console.log(" difference is :" +diff+ " the quotient is " +quotient)
